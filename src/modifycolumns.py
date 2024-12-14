e24207bc0288ebc2d4f8716b883aa66718b8145e
rdx40 <ivanjaison@gmail.com>
2024-12-14 23:34:24 +0530

    one more

---
+++ src/modifycolumns.py
@@ -0,0 +1,7 @@
+## Solution for 2884. Modify Columns (https://leetcode.com/problems/modify-columns/description/)
+
+
+import pandas as pd
+def modifySalaryColumn(employees: pd.DataFrame) -> pd.DataFrame:
+  employees['salary'] = employees['salary'] * 2
+  return employees
